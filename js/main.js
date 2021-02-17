5cdf433d03dcfdb474aafa9668c2232d3838f96d
Fede <fedebaez@gmail.com>
2021-02-17 11:12:24 +0100

    Añadido scroll horizontal a colecciones de pelis por js.

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Carousel buttons onclick events depending on view width
+(()=>{
+    let leftButtons = document.getElementsByClassName("carousel-control-prev");
+    for (button of leftButtons) {
+        button.onclick = ()=>{
+            let collection = document.getElementById(button.getAttribute("data-bs-target"));
+            let xscroll = collection.scrollLeft;
+            let rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            let filmwidth = 13.5 * rem;
+            let scrolln = (~~((document.body.scrollWidth-24-4*rem)/filmwidth));
+            scrolln = (scrolln == 0)?1:scrolln;
+            let scrollto = (~~(filmwidth*(~~((xscroll - filmwidth*scrolln)/filmwidth))));
+            scrollto = (scrollto<0)?0:scrollto;
+            collection.scroll({
+                top: 0,
+                left: scrollto,
+                behavior: 'smooth'
+              });;
+        };
+    }
+    let rightButtons = document.getElementsByClassName("carousel-control-next");
+    for (button of rightButtons) {
+        button.onclick = ()=>{
+            let collection = document.getElementById(button.getAttribute("data-bs-target"));
+            let xscroll = collection.scrollLeft;
+            let rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            let filmwidth = 13.5 * rem;
+            let scrolln = (~~((document.body.scrollWidth-24-4*rem)/filmwidth));
+            scrolln = (scrolln == 0)?1:scrolln;
+            let scrollto = (~~(filmwidth*(~~((xscroll + filmwidth*scrolln)/filmwidth))));
+            scrollto = (scrollto<0)?0:scrollto;
+            collection.scroll({
+                top: 0,
+                left: scrollto,
+                behavior: 'smooth'
+              });;
+        };
+    }
+})();
+
+// Load movies to home page
+(()=>{
+    
+})();
